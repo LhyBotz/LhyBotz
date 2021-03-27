fc0fcd7abede0d985989e56a9307abe157bccd75
LhyBotz <81456985+LhyBotz@users.noreply.github.com>
2021-03-27 02:43:26 -0300

    Add files via upload

---
+++ LhyBotz 1.0/komputer/suporte.java
@@ -0,0 +1,5 @@
+Pc = komputer * (LhyBotz + LhyBotz - Lhy)
+           + 4 * bot; // PREFER
+
+Android = /0/android/suporte.java * (LhyBotz + dark
+                       - LhyBotz) + 4 * bot; // AVOID
---
+++ LhyBotz 1.0/src/help.js
@@ -0,0 +1,89 @@
+const //database
+	return `
+╔══✪〘 INFO 〙✪══
+║
+╠➥LhyBotz
+╠➥ *1.0*
+╠➥ 𝐃𝐎𝐍𝐎: LhyEdits
+╠➥ 𝐒𝐓𝐀𝐓𝐔𝐒: ON
+║
+╠══✪〘 NOVIDADES 〙✪══
+║1 *${prefix}figu*
+║2 *${prefix}toimg*
+║3 *${prefix}meme*
+║4 *${prefix}memeindo*
+║5 *${prefix}tts*
+║6 *${prefix}lolih [on]*
+║7 *${prefix}nsfwloli [off]*
+║8 *${prefix}url2img*
+║9 *${prefix}leens [na legenda]*
+║10 *${prefix}wait [na legenda]*
+║11 *${prefix}setprefix*
+╠══✪〘 IMAGENS 〙✪══
+║
+║12 *${prefix}loli1*
+║13 *${prefix}hentai*
+║14 *${prefix}porno*
+║15 *${prefix}boanoite*
+║16 *${prefix}bomdia*
+║17 *${prefix}boatarde*
+║18 *${prefix}mia*
+║19 *${prefix}mia1*
+║20 *${prefix}mia2*
+║21 *${prefix}belle*
+║22 *${prefix}belle1*
+║23 *${prefix}belle2*
+║24 *${prefix}belle3*
+║25 *${prefix}akeno*
+║26 *${prefix}meme*   
+║27 *${prefix}lofi*
+║28 *${prefix}malkova*
+║29 *${prefix}canal*
+║30 *${prefix}nsfwloli1*
+║31 *${prefix}reislin*
+║
+╠══✪〘 INTELIGÊNCIA IA 〙✪══
+║
+║32 *${prefix}simih 1 (para ativar)*
+║33 *${prefix}simih 0 (para desativar)*
+║ *${prefix}simi (sua mensagem)*
+║
+╠══✪〘 PREMIUM 〙✪══
+║
+║34 *${prefix}dado*
+║35 *${prefix}cekvip*
+║36 *${prefix}delete*
+║37 *${prefix}modapk*
+║38 *${prefix}indo10*
+║39 *${prefix}qrcode*
+║40 *${prefix}chentai*
+║41 *${prefix}gcpf*
+║42 *${prefix}gbin*
+║43 *${prefix}pack*
+║44 *${prefix}destrava*
+║45 *${prefix}gpessoa*
+╠══✪〘 MAIS ALGUNS 〙✪══
+║
+║46 *${prefix}neko*
+║47 *${prefix}ttp [texto]*
+║48 *${prefix}testime*
+║49 *${prefix}tomp3*
+║50 *${prefix}animehug*
+║51 *${prefix}shoturl*
+║52 *${prefix}pokemon*
+║
+║
+╠══✪〘 𝗗𝗢𝗡𝗢 〙✪══
+║
+║ *NOME: LhyEdits
+║ *INSTA: LhyEdits
+║
+║  *"Peita ou respeita 🐊🚩*
+║
+║
+║
+╚═〘 LhyBotz 〙`
+}
+
+//Online.database
+
