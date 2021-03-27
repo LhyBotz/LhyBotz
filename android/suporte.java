e34f0fe0dea32312de248d15550bdacaafcf94b4
LhyBotz <81456985+LhyBotz@users.noreply.github.com>
2021-03-27 03:02:04 -0300

    Add files via upload

---
+++ android/suporte.java
@@ -0,0 +1,9 @@
+/*
+ * Classname             (LhyBotz)
+ *
+ * Version information   (1.0)
+ *
+ * Date                  (06/12,15:35)
+ *
+ * author                (LhyEdits)
+ * Copyright notice      (bot com suporte a Android/Java)
